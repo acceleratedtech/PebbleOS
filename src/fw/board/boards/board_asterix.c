ca594ef9ce1f37f9ec6a5d81b05b5e38a4c1a64c
Joshua Wise <joshua@accelerated.tech>
2025-07-07 21:29:11 -0700

    da6212: belt and suspenders: shut down peripheral if it is doing nothing

--- src/fw/board/boards/board_asterix.c
+++ src/fw/board/boards/board_asterix.c
@@ -227,6 +227,11 @@ void board_init(void) {
   i2c_init(&I2C_NPMC_IIC1_BUS);
   i2c_init(&I2C_IIC2_BUS);
 
+  uint8_t da7212_powerdown[] = { 0xFD /* SYSTEM_ACTIVE */, 0 };
+  i2c_use(I2C_DA7212);
+  i2c_write_block(I2C_DA7212, 2, da7212_powerdown);
+  i2c_release(I2C_DA7212);
+
 #if 0
   i2c_init(&I2C_PMIC_HRM_BUS);
 
